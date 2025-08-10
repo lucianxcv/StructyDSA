a7126b75801f1465781e2495306aa7f1e46cc0ad
lucian <lucianxcv@gmail.com>
2025-08-10 14:50:13 -0700

    sructy

--- 023getnodevalue.py
+++ 023getnodevalue.py
@@ -27,5 +27,20 @@ def get_node_value(head, index):
         current = current.next     # Move to the next node in the list
     
     return None                   # If index is out of range, return None
+print(get_node_value(a, 3))
 
-print(get_node_value(a, 1))
+"""
+def get_node_value(head, index):
+    if head is None:
+        return None                  # Base case: If the list is empty or we've reached the end, return None
+    
+    if index == 0:
+        return head.val              # Base case: If we've reached the target index, return the current node's value
+    
+    # Recursive step: Move to the next node and decrease index by 1
+    return get_node_value(head.next, index - 1)
+
+# How it works:
+# - Checks if the current node exists. If not, returns None since index is out of range.
+# - If index is 0, returns the current node
+"""
